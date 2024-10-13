bf5472fdb86cfeae6c4be8d3b8ae960de60a6bfd
winebarrel <sugawara@winebarrel.jp>
2024-10-13 20:26:49 +0900

    Rename job_test.queue to job_test.value

--- schema_test.sql
+++ schema_test.sql
@@ -4,7 +4,7 @@ CREATE TABLE job_test
 (
   job_id bigserial   NOT NULL,
   name   text        NOT NULL,
-  queue  text        NOT NULL,
+  value  text        NOT NULL,
 
   CONSTRAINT job_test_pkey PRIMARY KEY (job_id)
 );
