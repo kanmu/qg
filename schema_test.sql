4bbaa9d0d26f2359611df22e88c88c088bc4aa66
Genki Sugawara <genki.sugawara@kanmu.co.jp>
2024-10-14 10:45:05 +0900

    chore: Fix test table column type: bigserial -> bigint

--- schema_test.sql
+++ schema_test.sql
@@ -2,9 +2,9 @@ DROP TABLE IF EXISTS job_test;
 
 CREATE TABLE job_test
 (
-  job_id bigserial   NOT NULL,
-  name   text        NOT NULL,
-  value  text        NOT NULL,
+  job_id bigint NOT NULL,
+  name   text   NOT NULL,
+  value  text   NOT NULL,
 
   CONSTRAINT job_test_pkey PRIMARY KEY (job_id)
 );
